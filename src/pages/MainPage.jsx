bd3fc354955f94b521fad17e08e1a30070155d9d
jaemin <jaemin98@gmail.com>
2025-04-08 11:55:02 +0900

    MainPage 수정 - 이재민

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -2,18 +2,13 @@ import { Link } from "react-router-dom"
 import { useState } from "react";
 import BasicLayout from "../layouts/BasicLayout";
 
-
+// 메인 페이지 - 이재민
+// 아이템 -> 클릭시 아이템 상세 페이지로 이동 추후 변경
 function MainPage(){
   
-  
   // 이벤트 이미지를 관리하는 state
-  const [selectedEvent, setSelectedEvent] = useState(null);
+  const [selectedEvent, setSelectedEvent] = useState(1);
   
-  // 이벤트 클릭 시 상태 업데이트 함수
-  const handleEventClick = (eventNumber) => {
-    setSelectedEvent(eventNumber);
-  };
-
     return(
         <>
           <BasicLayout>
@@ -21,22 +16,22 @@ function MainPage(){
               <section>
                 <h2>주간 BEST</h2>
                 <div>
-                  <p>아이템1</p>
-                  <p>아이템2</p>
-                  <p>아이템3</p>
-                  <p>아이템4</p>
+                  <a>아이템1</a>
+                  <a>아이템2</a>
+                  <a>아이템3</a>
+                  <a>아이템4</a>
                 </div>
               </section>
               <section>
                 <h2>EVENT</h2>
                 <div>
-                  <p onClick={() => handleEventClick(1)}>이벤트1</p>
-                  <p onClick={() => handleEventClick(2)}>이벤트2</p>
-                  <p onClick={() => handleEventClick(3)}>이벤트3</p>
+                  <button onClick={() => setSelectedEvent(1)}>이벤트1</button>
+                  <button onClick={() => setSelectedEvent(2)}>이벤트2</button>
+                  <button onClick={() => setSelectedEvent(3)}>이벤트3</button>
                   <div>
-                    {selectedEvent === 1 && <img src="/event/event_banner1.jpg" alt="이벤트1 이미지" />}
-                    {selectedEvent === 2 && <img src="/event/event_banner2.jpg" alt="이벤트2 이미지" />}
-                    {selectedEvent === 3 && <img src="/event/event_banner.jpg" alt="이벤트3 이미지" />}
+                    {selectedEvent === 1 && <img src="/event/event_baner1.jpg" alt="이벤트1 이미지" />}
+                    {selectedEvent === 2 && <img src="/event/event_baner2.jpg" alt="이벤트2 이미지" />}
+                    {selectedEvent === 3 && <img src="/event/event_baner3.jpg" alt="이벤트3 이미지" />}
                   </div>
                 </div>
               </section>
