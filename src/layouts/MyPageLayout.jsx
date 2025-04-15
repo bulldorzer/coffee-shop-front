a3dea4da2adc85d53a40b81384eafec39ef31be4
jaemin <jaemin98@gmail.com>
2025-04-15 10:41:14 +0900

    회원 정보 수정 컴포넌트 - 이재민

--- src/layouts/MyPageLayout.jsx
+++ src/layouts/MyPageLayout.jsx
@@ -1,22 +1,25 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import SidebarMenu from "../component/menu/SideBarMenu";
+import {MemberProvider} from "../component/myPage/MemberContextComponent";
 
 // 마이페이지 기본 레이아웃 - 이재민
 const MyPageLayout = () => {
   return (
-    <div>
-      {/* 왼쪽 사이드 메뉴 */}
-      <aside>
-        <h2>마이 페이지</h2>
-        <SidebarMenu />
-      </aside>
+    <MemberProvider>
+      <div>
+        {/* 왼쪽 사이드 메뉴 */}
+        <aside>
+          <h2>마이 페이지</h2>
+          <SidebarMenu />
+        </aside>
 
-      {/* 오른쪽 콘텐츠 영역 */}
-      <main>
-        <Outlet />
-      </main>
-    </div>
+        {/* 오른쪽 콘텐츠 영역 */}
+        <main>
+          <Outlet />
+        </main>
+      </div>
+    </MemberProvider>
   );
 };
 
